ea7706e713f2aec16bd289404c31ccfd6dc8d79d
Zaharov Yaroslav <zahar190387@mail.ru>
2022-12-17 10:11:32 +0300

    work on mistakes

--- Zaharov Yaroslav/loop/index.js
+++ Zaharov Yaroslav/loop/index.js
@@ -1,15 +1,16 @@
 // 1. Написать программу, которая вычисляет сумму чисел от 1 до n. Значение n вводится с клавиатуры.
 // Проверить число n на корректность (чтобы это было число, чтобы оно не было меньше 1, чтобы это было не null).
 
-// let Number_n = prompt("Enter a number > 0");
-
-// if (!isNaN(Number_n) && Number_n === null && Number_n >= 1);
-// {
-//   let Summa = 0;
-//   for (let All_Numbers = 1; All_Numbers <= Number_n; All_Numbers++) {
-//     Summa = Summa + All_Numbers;
-//   }
-//   console.log(`Сумма от 1 до ${Number_n} равно ${Summa}`);
+// let number_n = prompt("Enter a number > 0");
+// number_n = Number(number_n);
+// let summa = 0;
+// if (number_n === NaN || number_n < 0 || number_n === null) {
+//   console.log("Enter a number > 0");
+// }
+// for (i = number_n; 0 < i; i--) {
+//   i = Number(i);
+//   summa = summa + i;
+//   console.log(`сумма от 1 до number_n ${summa}`);
 // }
 
 // 2. Написать программу, которая определяет количество
@@ -22,38 +23,34 @@
 // Отрицательных: 5 шт.
 // Нулей: 3 шт.»
 
-// let NumberTimesInput = prompt("Enter you numbers");
-// NumbersPlus = 0;
-// NumbersMinus = 0;
-// NumbersZero = 0;
+// let numberTimesInput = prompt("Enter you numbers");
+// numbersPlus = 0;
+// numbersMinus = 0;
+// numbersZero = 0;
 
-// for (let x = 1; x <= NumberTimesInput; x++) {
-//   let = You_Numbers = prompt("Enter you numbers");
-//   if (You_Numbers > 0) {
-//     NumbersPlus++;
+// for (let x = 1; x <= numberTimesInput; x++) {
+//   let = you_Numbers = prompt("Enter you numbers");
+//   if (you_Numbers > 0) {
+//     numbersPlus++;
 //   }
-//   if (You_Numbers < 0) {
-//     NumbersMinus++;
+//   if (you_Numbers < 0) {
+//     numbersMinus++;
 //   }
-//   if (You_Numbers === 0) {
-//     NumbersZero++;
+//   if (you_Numbers === 0) {
+//     numbersZero++;
 //   }
 // }
-// console.log("положительных:" + NumbersPlus);
-// console.log("отрицательных:" + NumbersMinus);
-// console.log("нулей:" + NumbersZero);
+// console.log("положительных:" + numbersPlus);
+// console.log("отрицательных:" + numbersMinus);
+// console.log("нулей:" + numbersZero);
 
 // 3. Написать программу, которая выводит на экран
 // двузначные числа, которые делятся на 4, но не
 // делятся на 6.
 
-// for (let x = 10; x < 30; x++) {
-//   if (x % 4 === 0) {
+// for (let x = 1; x < 30; x++) {
+//   if (x % 4 === 0 && x % 6 !== 0) {
 //     console.log(x);
-//   }
-//   if (x % 6 !== 0) {
-//     console.log;
-//     x;
 //   }
 // }
 
@@ -63,16 +60,16 @@
 // тогда, когда нажали «отмена», после чего выводится
 // среднее арифметическое.
 
-let sum = 0;
-let Numbers_Times = 0;
+// let sum = 0;
+// let numbers_Times = 0;
 
-for (let i = 0; ; ) {
-  let Number_user = prompt("enter  you numbers");
-  if (Number_user === null) {
-    break;
-  }
-  Number_user = +Number_user;
-  sum += Number_user;
-  Numbers_Times++;
-}
-console.log("average" + sum / Numbers_Times);
+// for (let i = 0; ; ) {
+//   let number_user = prompt("enter  you numbers");
+//   if (number_user === null) {
+//     break;
+//   }
+//   number_user = +number_user;
+//   sum += number_user;
+//   numbers_Times++;
+// }
+// console.log("average" + sum / numbers_Times);

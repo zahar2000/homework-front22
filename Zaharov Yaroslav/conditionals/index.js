9a9ae89b0a41e69af701be66fa987519acc64677
Zaharov Yaroslav <zahar190387@mail.ru>
2022-12-17 10:32:32 +0300

    work on mistakes with conditionals

--- Zaharov Yaroslav/conditionals/index.js
+++ Zaharov Yaroslav/conditionals/index.js
@@ -9,39 +9,50 @@
 // выводится такое сообщение: «Федот и Митрофан
 // одного возраста».
 
-let Fedot = prompt("enter  you age");
-let Mitrofan = prompt("enter  you age");
-let ageDiferenceFedot = Fedot - Mitrofan;
-let ageDiferenceMitrofan = Mitrofan - Fedot;
+// 1. Создать программу в которой сравнивается возраст
+// двух человек (Федота и Митрофана).
+// Если возраст Федота больше чем возраст Митрофана,
+// то должно выводится сообщение, в котором указана
+// разница в возрасте: «Федот старше Митрофана на 3 ».
+// Если возраст Митрофана больше, то выводится такое
+// сообщение: «Митрофан старше Федота на 5».
+// Если возраст обоих человек один и тот же, то
+// выводится такое сообщение: «Федот и Митрофан
+// одного возраста».
 
-if (Fedot === null || Mitrofan === null) {
-  alert("you cancelled!");
-  Fedot = Number(Fedot);
-  Mitrofan = Number(Mitrofan);
-} else {
-  if (isNaN(Fedot) || isNaN(Mitrofan)) {
-    alert("enter numbers!");
-  } else if (Fedot > Mitrofan) {
-    console.log(`Федот старше Митрофана на ${ageDiferenceFedot}`);
-  } else if (Mitrofan > Fedot) {
-    console.log(`Митрофан старше Федота на  ${ageDiferenceMitrofan}`);
-  } else if (Mitrofan === Fedot) {
-    console.log("«Федот и Митрофан одного возраста");
-  }
-}
+// let fedot = prompt("enter  you age");
+// let mitrofan = prompt("enter  you age");
+// let ageDiferenceFedot = fedot - mitrofan;
+// let ageDiferenceMitrofan = mitrofan - fedot;
 
-// 2. Пользователь вводит ТРИ числа
-// вывести в консоль наибольшее из них
+// if (fedot === null || mitrofan === null) {
+//   alert("you cancelled!");
+//   fedot = Number(fedot);
+//   mitrofan = Number(mitrofan);
+// } else {
+//   if (isNaN(fedot) || isNaN(mitrofan)) {
+//     alert("enter numbers!");
+//   } else if (fedot > mitrofan) {
+//     console.log(`Федот старше Митрофана на ${ageDiferenceFedot}`);
+//   } else if (mitrofan > fedot) {
+//     console.log(`Митрофан старше Федота на  ${ageDiferenceMitrofan}`);
+//   } else if (mitrofan === fedot) {
+//     console.log("«Федот и Митрофан одного возраста");
+//   }
+// }
 
-let NumberX = prompt("enter number");
-let NumberY = prompt("enter number");
-let NumberZ = prompt("enter number");
-if (NumberX > NumberY && NumberX > NumberZ) {
-  console.log(NumberX);
-}
-if (NumberY > NumberZ && NumberY > NumberX) {
-  console.log(NumberY);
-}
-if (NumberZ > NumberX && NumberZ > NumberY) {
-  console.log(NumberZ);
-}
+// // 2. Пользователь вводит ТРИ числа
+// // вывести в консоль наибольшее из них
+
+// let numberX = prompt("enter number");
+// let numberY = prompt("enter number");
+// let numberZ = prompt("enter number");
+// if (numberX > numberY && numberX > numberZ) {
+//   console.log(NumberX);
+// }
+// if (numberY > numberZ && numberY > numberX) {
+//   console.log(NumberY);
+// }
+// if (numberZ > numberX && numberZ > numberY) {
+//   console.log(numberZ);
+// }
--- Zaharov Yaroslav/loop/index.js
+++ Zaharov Yaroslav/loop/index.js
@@ -1,3 +1,50 @@
+// 1. Создать программу в которой сравнивается возраст
+// двух человек (Федота и Митрофана).
+// Если возраст Федота больше чем возраст Митрофана,
+// то должно выводится сообщение, в котором указана
+// разница в возрасте: «Федот старше Митрофана на 3 ».
+// Если возраст Митрофана больше, то выводится такое
+// сообщение: «Митрофан старше Федота на 5».
+// Если возраст обоих человек один и тот же, то
+// выводится такое сообщение: «Федот и Митрофан
+// одного возраста».
+
+// let fedot = prompt("enter  you age");
+// let mitrofan = prompt("enter  you age");
+// let ageDiferenceFedot = fedot - mitrofan;
+// let ageDiferenceMitrofan = mitrofan - fedot;
+
+// if (fedot === null || mitrofan === null) {
+//   alert("you cancelled!");
+//   fedot = Number(fedot);
+//   mitrofan = Number(mitrofan);
+// } else {
+//   if (isNaN(fedot) || isNaN(mitrofan)) {
+//     alert("enter numbers!");
+//   } else if (fedot > mitrofan) {
+//     console.log(`Федот старше Митрофана на ${ageDiferenceFedot}`);
+//   } else if (mitrofan > fedot) {
+//     console.log(`Митрофан старше Федота на  ${ageDiferenceMitrofan}`);
+//   } else if (mitrofan === fedot) {
+//     console.log("«Федот и Митрофан одного возраста");
+//   }
+// }
+
+// // 2. Пользователь вводит ТРИ числа
+// // вывести в консоль наибольшее из них
+
+// let numberX = prompt("enter number");
+// let numberY = prompt("enter number");
+// let numberZ = prompt("enter number");
+// if (numberX > numberY && numberX > numberZ) {
+//   console.log(NumberX);
+// }
+// if (numberY > numberZ && numberY > numberX) {
+//   console.log(NumberY);
+// }
+// if (numberZ > numberX && numberZ > numberY) {
+//   console.log(numberZ);
+// }
 // 1. Написать программу, которая вычисляет сумму чисел от 1 до n. Значение n вводится с клавиатуры.
 // Проверить число n на корректность (чтобы это было число, чтобы оно не было меньше 1, чтобы это было не null).
 
@@ -73,3 +120,4 @@
 //   numbers_Times++;
 // }
 // console.log("average" + sum / numbers_Times);
+//

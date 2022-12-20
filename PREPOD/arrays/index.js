88ca18352e42fad93deceff7697eae7e19f5d6c1
Web <web@it.step>
2022-12-20 18:36:47 +0300

    arrays hw

---
+++ PREPOD/arrays/index.js
@@ -0,0 +1,84 @@
+
+// const good1 = 'apple';
+// const good2 = 'potato';
+// const good3 = 'orange';
+// const good4 = 'pepper';
+// const good5 = 'cucumber';
+
+// const cart = ['apple', 'potato', 'orange', 'pepper', 'cucumber'];
+// let cart2 = ['apple', 'potato', 'orange', 'pepper', 'cucumber'];
+
+// console.log(cart);
+// cart[cart.length] = 'banana';
+// cart[cart.length] = 'kolbasa';
+// console.log(cart);
+
+// let cart = ['apple', 'potato', 'orange', 'pepper', 'cucumber'];
+
+// cart.length = 3; // не записывать ничего в .length!!!
+
+// cart = null;
+// let userCart = cart;
+// userCart[0] = 'samsung';
+
+// console.log(userCart);
+
+
+
+
+// const arr = [ 
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+
+
+// console.log( [ 
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ][0][2] );
+
+
+
+let cart = ['apple', 'potato', 'orange', 'pepper', 'cucumber'];
+
+// cart[0]
+// cart[1]
+// cart[2]
+// cart[3]
+// cart[4]
+
+
+// for (let i = 0; i < cart.length; i++) {
+//     console.log(`i bought ${cart[i]}`);
+// }
+
+// const arr =[ 
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+//     for (let j = 0; j < arr[i].length; j++) {
+//         console.log( arr[i][j] );
+//     }
+// }
+
+// 5. Создать массив, элементы которого равны
+// элементам исходного массива, но с
+// противоположными знаками. (Длина исходного
+// массива должна быть не меньше 10ти элементов).
+
+const arr = [2,5,-7,92,-19,52,-23,61,-74,45];
+const resultArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+    resultArr[i] = -arr[i];
+}
+
+console.log(arr);
+console.log(resultArr);
